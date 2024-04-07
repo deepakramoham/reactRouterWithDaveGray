ddd73a243f6e5ff4e035769a86edf02e71ee79e0
Deepak Janardhanan <deepakramoham@gmail.com>
2024-04-07 21:15:06 +0530

    04-ReactCustomHooksWithAxiosAsync_useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import { useState, useEffect } from "react";
 import { format } from "date-fns";
 import api from "./api/posts";
 import EditPost from "./components/EditPost";
+import useWindowSize from "./hooks/useWindowSize";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -21,6 +22,7 @@ function App() {
   const [editTitle, setEditTitle] = useState("");
   const [editBody, setEditBody] = useState("");
   const history = useHistory();
+  const {width} = useWindowSize();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -98,7 +100,7 @@ function App() {
   };
   return (
     <div className="App">
-      <Header title="React JS Blog" />
+      <Header title="React JS Blog" width={width} />
       <Nav search={search} setSearch={setSearch} />
       <Switch>
         <Route exact path="/">
